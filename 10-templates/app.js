dc9b036f4180581673e82780930fb99b2a9790f5
lizaszauder <liza.szauder@trendency.hu>
2023-01-09 00:06:26 +0100

    82-lesson

---
+++ 10-templates/app.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const path = require('path')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const rootDir = require('./util/path')
+
+const adminData = require ('./routes/admin')
+const shopRoutes = require ('./routes/shop')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', 'views') // default
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(rootDir, 'public')))
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, '/views', '404.html'))
+})
+
+app.listen(3000)
