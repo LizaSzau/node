878fb581f50a54285d6ff8e38cca26606f5fc9f8
lizaszauder <liza.szauder@trendency.hu>
2023-01-10 21:34:57 +0100

    92-lesson

---
+++ 12-templates-ejs/app.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const path = require('path')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const rootDir = require('./util/path')
+
+const adminData = require ('./routes/admin')
+const shopRoutes = require ('./routes/shop')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'views') // default
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(rootDir, 'public')))
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.render('404', {pageTitle: '404'})
+})
+
+app.listen(3000)
